ace56fe4576c424c3efd2df9e49b13f588cc6762
Freewind <nowindlee@gmail.com>
2020-04-23 21:23:29 +0800

    typescript-create-partial-of-another-type-demo

--- README.md
+++ README.md
@@ -1,18 +1,8 @@
-TypeScript Hello World Demo
+TypeScript Create Partial of Another Type Demo
 ===========================
 
 ```
 npm install
 npm run demo
-```
-
-Note:
-
-`esModuleInterop` is recommended to set to `true`,
-since we can have consistent importing syntax with babel,
-always use:
-
-```
-import some from 'some'
 ```
 
--- hello.ts
+++ hello.ts
@@ -1,1 +1,42 @@
-console.log('Hello');
+type BigType = {
+  aaa: string,
+  bbb?: number,
+  ccc: boolean[],
+  extra?: {
+    [key in string]?: string
+  },
+  nested1: {
+    nested2: {
+      nested3: {
+        [key in string]?: string
+      }
+    }
+  }
+}
+
+type RecursivePartial<T> = {
+  [P in keyof T]?:
+  T[P] extends (infer U)[] ? RecursivePartial<U>[] :
+    T[P] extends object ? RecursivePartial<T[P]> :
+      T[P];
+};
+
+function partialOf<T>() {
+  return <X extends RecursivePartial<T>>(x: X): X => {
+    return x;
+  }
+}
+
+const partialOfBigType = partialOf<BigType>();
+
+const partial: {
+  aaa: string;
+  extra: { ddd: string };
+  nested1: { nested2: {} }
+} = partialOfBigType({
+  aaa: 'aaa11',
+  extra: {'ddd': '222'},
+  nested1: {nested2: {}}
+})
+
+console.log(partial)
